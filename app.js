3bbf96002f9b8b2ed5c857215cc29df7b09051cd
talli <127283670+ebistu@users.noreply.github.com>
2024-07-09 22:34:14 +0300

    finished exercise

--- app.js
+++ app.js
@@ -1,57 +1,44 @@
 
 playGame();
+
+
+
+
 function playGame(){
     let humanScore = 0;
     let computerScore = 0;
-    for(i = 0; i < 4; i++){
-        playRound(getHumanChoice(), getComputerChoice());
-    }    
-    console.log("Your score: " + humanScore + " Computer score: " + computerScore);
-    function playRound(humanChoice, computerChoice){
-        switch(humanChoice){
-            case 'rock':
-                if(computerChoice === 'rock'){
-                    console.log("You tie!");
-                }else if(computerChoice === 'paper'){
-                    console.log("You lose! paper beats rock");
-                    computerScore = computerScore + 1;
-                }else if(computerChoice === 'scissors'){
-                    console.log("You win! rock beats scissors");
-                    humanScore = humanScore + 1;
-                }
-            case 'paper':
-                if(computerChoice === 'paper'){
-                    console.log("You tie!");
-                }else if(computerChoice === 'scissors'){
-                    console.log("You lose! scissors beats paper");
-                    computerScore = computerScore + 1;
-                }else if(computerChoice === 'rock'){
-                    console.log("You win! paper beats rock");
-                    humanScore = humanScore + 1;
-                }
-            case 'scissors':
-                if(computerChoice === 'scissors'){
-                    console.log("You tie!");
-                }else if(computerChoice === 'rock'){
-                    console.log("You lose! rock beats scissors");
-                    computerScore = computerScore + 1;
-                }else if(computerChoice === 'paper'){
-                    console.log("You win! scissors beats paper");
-                    humanScore = humanScore + 1;
-                }
+
+    for(i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        let outcome = checkGame(humanChoice, computerChoice);
+        if(outcome === "tie"){
+            console.log("You both chose " + humanChoice + ".");
+        }
+        if(outcome === "lose"){
+            console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+            computerScore = computerScore + 1;
+        }
+        if(outcome === "win"){
+            console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+            humanScore = humanScore + 1;
         }
     }
+    console.log("Your score is " + humanScore + ". Computers score is " + computerScore + ".");
 }
 
 function checkGame(humanChoice, computerChoice){
-    switch(humanChoice){
-        case 'rock':
-            if(computerchoice === humanChoice){
-                return "tie";
-            } else if(computerChoice === "paper"){
-                return "lose";
-            } else if(computerChoice === "scissors")
+    if(computerChoice === humanChoice){
+        return "tie";
     }
+    if(humanChoice === "rock" && computerChoice === "paper"){
+        return "lose";
+    }
+    if(humanChoice === "paper" && computerChoice === "scissors"){
+        return "lose";
+    }
+    return "win";
 }
 
 
@@ -59,25 +46,23 @@ function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3);
     switch(computerChoice){
         case 0:
-            return 'rock';
+            return "rock";
         case 1:
-            return 'paper';
-        case 3:
-            return 'scissors';
+            return "paper";
+        case 2:
+            return "scissors";
     }
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt("What's your pick?");
-    humanChoice = humanChoice.toLowerCase();
-    switch (humanChoice){
-        case 'rock':
-            return humanChoice;
-        case 'paper':
-            return humanChoice;
-        case 'scissors':
-            return humanChoice;
-        default:
-            return getHumanChoice();
+    while(true){
+        let humanChoice = prompt("What's your pick?");
+        humanChoice = humanChoice.toLowerCase();
+        switch (humanChoice){
+            case 'rock':
+            case 'paper':
+            case 'scissors':
+                return humanChoice;
+        }
     }
 }
