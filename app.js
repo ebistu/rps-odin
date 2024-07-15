7b59d033fc0228fb5d62400e656662a5140fc880
ebistu <127283670+ebistu@users.noreply.github.com>
2024-07-15 21:27:20 +0300

    ui done

--- app.js
+++ app.js
@@ -1,44 +1,63 @@
 
-playGame();
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors"); 
+const playedRound = document.querySelector("#playedRound");
+const resultDiv = document.querySelector("#result");
 
+let humanScore = 0;
+let computerScore = 0;
 
-
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for(i = 0; i < 5; i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        console.log(computerChoice);
-        let outcome = checkGame(humanChoice, computerChoice);
-        if(outcome === "tie"){
-            console.log("You both chose " + humanChoice + ".");
-        }
-        if(outcome === "lose"){
-            console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
-            computerScore = computerScore + 1;
-        }
-        if(outcome === "win"){
-            console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
-            humanScore = humanScore + 1;
-        }
+function playRound(human){
+    let computerChoice = getComputerChoice();
+    let roundPlayed = checkGame(human, computerChoice);
+    if(roundPlayed > 0){
+        humanScore++;
     }
-    console.log("Your score is " + humanScore + ". Computers score is " + computerScore + ".");
+    if(roundPlayed < 0){
+        computerScore++;
+    }
+    playedRound.textContent = returnRoundStatement(roundPlayed, human, computerChoice);
+    resultDiv.textContent = "Your score: " + humanScore + ", " + "computers' score: " + computerScore + ".";
+}
+
+rockBtn.addEventListener("click", () => {
+    playRound(rockBtn.id);
+});
+paperBtn.addEventListener("click", () => {
+    playRound(paperBtn.id);
+});
+scissorsBtn.addEventListener("click", () => {
+    playRound(scissorsBtn.id);
+});
+
+
+function returnRoundStatement(result, humanChoice, computerChoice){
+    switch(result){
+        case -1:
+            return `You lose! ${computerChoice} beats ${humanChoice}`;
+        case 0:
+            return `You both chose ${computerChoice}.`;
+        case 1:
+            return `You win! ${humanChoice} beats ${computerChoice}`;
+    }
 }
 
 function checkGame(humanChoice, computerChoice){
+
     if(computerChoice === humanChoice){
-        return "tie";
+        return 0;
     }
     if(humanChoice === "rock" && computerChoice === "paper"){
-        return "lose";
+        return -1;
     }
     if(humanChoice === "paper" && computerChoice === "scissors"){
-        return "lose";
+        return -1;
     }
-    return "win";
+    if(humanChoice === "scissors" && computerChoice === "rock"){
+        return -1;
+    }
+    return 1;
 }
 
 
@@ -65,4 +84,29 @@ function getHumanChoice(){
                 return humanChoice;
         }
     }
+}
+
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for(i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        let outcome = checkGame(humanChoice, computerChoice);
+        if(outcome === "tie"){
+            console.log("You both chose " + humanChoice + ".");
+        }
+        if(outcome === "lose"){
+            console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+            computerScore = computerScore + 1;
+        }
+        if(outcome === "win"){
+            console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+            humanScore = humanScore + 1;
+        }
+    }
+    console.log("Your score is " + humanScore + ". Computers score is " + computerScore + ".");
 }
